0e945eb4789a0abaa272a459c5195c767ffbc636
SuperWallaby <colton950901@gmail.com>
2020-04-30 15:41:14 +0900

    before eject

--- src/pages/customerList/MainAdmin.tsx
+++ src/pages/customerList/MainAdmin.tsx
@@ -1,10 +1,27 @@
 import React, { Fragment } from 'react';
-import { JDbutton, JDalign, JDselect } from '@janda-com/front';
-import '@janda-com/front/build/scss/all.scss';
+import { JDbutton, JDalign, JDselect, JDradioButton, useRadioButton } from '@janda-com/front';
 import './MainAdmin.scss';
 import CustomerCard from './components/CustomerCard';
 
+const viewOption = [
+	{
+		label: '업소별 보기',
+		value: 'HOUSE',
+		iconProp: {
+			icon: 'addCircle'
+		}
+	},
+	{
+		label: '회원별 보기',
+		value: 'PERSON',
+		iconProp: {
+			icon: 'addCircle'
+		}
+	}
+];
+
 const MainAdmin: React.FC<any> = ({}) => {
+	const radioButtonHook = useRadioButton([ 'HOUSE' ], viewOption);
 	return (
 		<div className="superMain">
 			<div className="container container--full">
@@ -12,16 +29,22 @@ const MainAdmin: React.FC<any> = ({}) => {
 					<Fragment>
 						<div className="docs-section__box">
 							<div>
-								<JDalign>
-									<JDbutton label={'결제내역'} />
-									<JDbutton
-										label={'단체알림'}
-										iconProp={{
-											icon: 'bell'
-										}}
-									/>
-									<JDselect />
-									<JDmulitBox />
+								<JDalign
+									flex={{
+										between: true
+									}}
+								>
+									<div>
+										<JDbutton label={'결제내역'} />
+										<JDbutton
+											label={'단체알림'}
+											iconProp={{
+												icon: 'bell'
+											}}
+										/>
+									</div>
+									<JDselect z={2} />
+									<JDradioButton {...radioButtonHook} />
 								</JDalign>
 								<CustomerCard />
 								<CustomerCard />
